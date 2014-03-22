a23a32080fda7a555e8e1f3f3025acfc089a973c
Lars Andersen <larsmew@gmail.com>
2014-03-22 14:55:47 +0100

    Made code follow the flake8 standard

--- swarm_breaker2.py
+++ swarm_breaker2.py
@@ -9,15 +9,13 @@ from optparse import OptionParser
 from operator import itemgetter
 from collections import deque
 #from igraph import *
-import tempfile
-import subprocess
 import sys
 import time
 import os
 
-manuelCut = False #Default: False
-#THRESHOLD = 0
+manuelCut = False  # Default: False
 PARAMETER = True
+
 
 class Adjacency_list(object):
     neighbours = []
@@ -30,41 +28,43 @@ class Adjacency_list(object):
     seed = False
     newParent = -1
 
-    # Initializer 
+    # Initializer
     def __init__(self, neighbours):
         self.neighbours = neighbours
+
 
 def make_adjlist(neighbours):
     adjacency_list = Adjacency_list(neighbours)
     return adjacency_list
 
+
 def option_parse():
     """
     Parse arguments from command line.
     """
-    desc = """Cut swarms."""
+    desc = """Break swarms."""
 
-    parser = OptionParser(usage="usage: %prog --input_file filename",
-        description = desc,
-        version = "%prog version 1.0")
+    parser = OptionParser(usage="usage: %prog --data_file filename",
+                          description=desc,
+                          version="%prog version 1.0")
 
     parser.add_option("-f", "--fasta_file",
-        metavar="<FILENAME>",
-        action="store",
-        dest="fasta_file",
-        help="set <FILENAME> as fasta file.")
+                      metavar="<FILENAME>",
+                      action="store",
+                      dest="fasta_file",
+                      help="set <FILENAME> as fasta file.")
 
     parser.add_option("-s", "--swarm_file",
-        metavar="<FILENAME>",
-        action="store",
-        dest="swarm_file",
-        help="set <FILENAME> as swarm file.")
-              
-    parser.add_option("-i", "--input_file",
-        metavar="<FILENAME>",
-        action="store",
-        dest="input_file",
-        help="set <FILENAME> as input file.")
+                      metavar="<FILENAME>",
+                      action="store",
+                      dest="swarm_file",
+                      help="set <FILENAME> as swarm file.")
+
+    parser.add_option("-d", "--data_file",
+                      metavar="<FILENAME>",
+                      action="store",
+                      dest="input_file",
+                      help="set <FILENAME> as input file.")
 
     (options, args) = parser.parse_args()
 
@@ -79,7 +79,7 @@ def buildGraph(fasta_file, swarm_file, input_file):
     with open(swarm_file, "rU") as swarm_file:
         for line in swarm_file:
             amplicons = [(amplicon.split("_")[0], int(amplicon.split("_")[1]))
-                     for amplicon in line.strip().split(" ")]
+                         for amplicon in line.strip().split(" ")]
 
     amplicon_index = {amplicon[0]: i for (i, amplicon) in enumerate(amplicons)}
     #nodeNames = [name[0] for name in amplicons]
@@ -87,13 +87,13 @@ def buildGraph(fasta_file, swarm_file, input_file):
 
     # Initialize graph structure
     G = [make_adjlist([]) for i in range(len(amplicon_index))]
-    print "Network size:",len(G)
+    print "Network size:", len(G)
 
     # Insert name and abundance for each node
     for i in range(len(G)):
-        G[i].name = amplicons[i][0] # The nodes hashed name
-        G[i].abundance = int(amplicons[i][1]) # the node's abundance
-        G[i].num = i # ID in graph
+        G[i].name = amplicons[i][0]  # The nodes hashed name
+        G[i].abundance = int(amplicons[i][1])  # the node's abundance
+        G[i].num = i  # ID in graph
 
     if input_file:
         # Create list of neighbours
@@ -109,16 +109,18 @@ def buildGraph(fasta_file, swarm_file, input_file):
     else:
         print "ERROR: NO INPUT FILE OR FASTA FILE GIVEN"
         sys.exit(0)
-        
+
     return G
-    
-    
-def manualCutter(G,possibleCuts):
+
+
+def manualCutter(G, possibleCuts):
     print
     print "ENTER MANUAL CUTTING MODE:"
-    print "Instructions: To perform cut press y (yes) or enter, to keep edge press n (no)"
+    print "Instructions: To perform cut press y (yes) or enter, ",
+    print "to keep edge press n (no)"
     print "Possible cuts:"
-    print "[Cand for cut]    [Cand abundance]    [Closest root]    [Roots abundance]"
+    print "[Cand for cut]    [Cand abundance]    ",
+    print "[Closest root]    [Roots abundance]"
     space1 = 18
     space2 = 20
     space3 = 18
@@ -129,19 +131,23 @@ def manualCutter(G,possibleCuts):
             len2 = space1-len(str([G[node].num for node in edge]))
             len3 = space2-len(str([G[node].abundance for node in edge]))-4
             len4 = space3-len(str([G[node].belongingRoot for node in edge]))-1
-            print " ",[G[node].num for node in edge]," "*len2,[G[node].abundance for node in edge]," "*len3,[G[node].belongingRoot for node in edge]," "*len4,[G[G[node].belongingRoot].abundance for node in edge],
+            print " ", [G[node].num for node in edge], " " * len2, \
+                [G[node].abundance for node in edge], " " * len3, \
+                [G[node].belongingRoot for node in edge], " " * len4, \
+                [G[G[node].belongingRoot].abundance for node in edge],
             answer = raw_input("cut? ")
             if not answer == "n" or answer == "no" or answer == "nn":
                 finalCuts.append(edge)
     return finalCuts
 
-def prettyPrintCuts(G,finalCuts,tim):
-    '''
+
+def prettyPrintCuts(G, finalCuts, tim):
+    """
     A bad trial to give a nice output of the cuts.
-    '''
-    print
-    print "Final cuts:"
-    print "[Cand for cut]    [Cand abundance]    [Closest root]    [Roots abundance]"
+    """
+    print "\nFinal cuts:"
+    print "[Cand for cut]    [Cand abundance]    ",
+    print "[Closest root]    [Roots abundance]"
     space1 = 18
     space2 = 20
     space3 = 18
@@ -149,17 +155,18 @@ def prettyPrintCuts(G,finalCuts,tim):
         len2 = space1-len(str([G[node].num for node in edge]))
         len3 = space2-len(str([G[node].abundance for node in edge]))-4
         len4 = space3-len(str([G[node].belongingRoot for node in edge]))-1
-        print " ",[G[node].num for node in edge]," "*len2,[G[node].abundance for node in edge]," "*len3,[G[node].belongingRoot for node in edge]," "*len4,[G[G[node].belongingRoot].abundance for node in edge]
+        print " ", [G[node].num for node in edge], " " * len2, \
+            [G[node].abundance for node in edge], " " * len3, \
+            [G[node].belongingRoot for node in edge], " " * len4, \
+            [G[G[node].belongingRoot].abundance for node in edge]
         #print [G[node].name for node in edge]
-    print "Number of final cuts:",len(finalCuts)
-    print "Time:",tim
-    print
-    
-"""
-Two versions for rewire nodes' belonging root, if under treshold.
-Testing which is better - rewireFromRoot most promising.
-"""
-def rewireFromNode(G,node,threshold):
+    print "Number of final cuts:", len(finalCuts)
+    print "Time:", tim, "\n"
+
+
+### Two versions for rewire nodes' belonging root, if under treshold.
+### Testing which is better - rewireFromRoot most promising.
+def rewireFromNode(G, node, threshold):
     """
     Rewire node from breaking point, if belonging root is below threshold.
     """
@@ -170,7 +177,7 @@ def rewireFromNode(G,node,threshold):
         for neighbour in G[queue.popleft()].neighbours:
             curAbundance = G[G[neighbour].belongingRoot].abundance
             if curAbundance > threshold \
-            and curAbundance > belongingRootAbundance:
+               and curAbundance > belongingRootAbundance:
                 newParent = neighbour
                 belongingRootAbundance = curAbundance
             else:
@@ -179,9 +186,9 @@ def rewireFromNode(G,node,threshold):
     G[node].belongingRoot = G[newParent].belongingRoot
 
 
-def rewireFromRoot(G,node,threshold):
+def rewireFromRoot(G, node, threshold):
     """
-    Rewire node (at break point) from belonging root, 
+    Rewire node (at break point) from belonging root,
     if belonging root is below threshold.
     """
     newParent = -1
@@ -191,7 +198,7 @@ def rewireFromRoot(G,node,threshold):
         for neighbour in G[queue.popleft()].neighbours:
             curAbundance = G[G[neighbour].belongingRoot].abundance
             if curAbundance > threshold \
-            and curAbundance > belongingRootAbundance:
+               and curAbundance > belongingRootAbundance:
                 newParent = neighbour
                 belongingRootAbundance = curAbundance
             else:
@@ -200,7 +207,7 @@ def rewireFromRoot(G,node,threshold):
     G[node].belongingRoot = G[newParent].belongingRoot
 
 
-def assignParent(G,THRESHOLD):
+def assignParent(G, THRESHOLD):
     """
     Assign biggest neighbour as parent for each node in graph
     """
@@ -226,7 +233,7 @@ def assignParent(G,THRESHOLD):
             else:
                 node.parent = -2  # Has no parent
                 node.belongingRoot = node.num
-                
+
     # Find name of node - Test purpose
     # if node.abundance == 2708:
     #     print "1:",node.num
@@ -236,26 +243,27 @@ def assignParent(G,THRESHOLD):
 
 def findPossibleCuts(G):
     """
-    Find possible cuts by finding edges where no nodes is 
+    Find possible cuts by finding edges where no nodes is
     pointing from or to it.
     """
     tim = time.clock()
-    
+
     possibleCuts = []
     for node in G:
         for neighbour in node.neighbours:
             if node.num < neighbour:
-                if node.parent != neighbour and G[neighbour].parent != node.num:
-                    possibleCuts.append((node.num,neighbour))
+                if node.parent != neighbour and \
+                   G[neighbour].parent != node.num:
+                    possibleCuts.append((node.num, neighbour))
     #print "Possible cuts:\n",possibleCuts
-    print "Number of possible cuts:",len(possibleCuts)
+    print "Number of possible cuts:", len(possibleCuts)
     #prettyPrintCuts(G,possibleCuts,tim)
-    
-    print "Time:",time.clock()-tim
+
+    print "Time:", time.clock() - tim
     return possibleCuts
 
 
-def findBelongingRoot(G,possibleCuts):
+def findBelongingRoot(G, possibleCuts):
     """
     Find corresponding root for each node in possible cuts
     """
@@ -266,7 +274,7 @@ def findBelongingRoot(G,possibleCuts):
                 #path = [node]
                 while G[currentNode.parent].belongingRoot == -1:
                     #path.append(currentNode.parent)
-                    
+
                     # Skip pair of nodes pointing at each other
                     if G[currentNode.parent].parent == currentNode.num:
                         G[currentNode.parent].belongingRoot = currentNode.num
@@ -277,75 +285,80 @@ def findBelongingRoot(G,possibleCuts):
                 G[node].belongingRoot = G[currentNode.parent].belongingRoot
 
 
-def rewireNode(G,possibleCuts,THRESHOLD):
+def rewireNode(G, possibleCuts, THRESHOLD):
     """
-    "Rewire" nodes connected to a parent with belonging root 
+    "Rewire" nodes connected to a parent with belonging root
     that has abundance lower than threshold
     """
     threshold = THRESHOLD if THRESHOLD >= 1 else THRESHOLD*len(G)
     for edge in possibleCuts:
         for node in edge:
             if G[G[node].belongingRoot].abundance < threshold:
-                #rewireFromNode(G,node,threshold)
-                rewireFromRoot(G,node,threshold)
+                #rewireFromNode(G, node, threshold)
+                rewireFromRoot(G, node, threshold)
 
 
-def findFinalCuts(G,possibleCuts,THRESHOLD,tim):
+def findFinalCuts(G, possibleCuts, THRESHOLD, tim):
     """
-    Find final cuts, either by manually deciding the cuts or by 
+    Find final cuts, either by manually deciding the cuts or by
     using a parameter, or only using the threshold as tiebreaker.
     """
     finalCuts = []
     # If manual cut on, the user gets to decide which edges will be cut.
     if manuelCut:
-        finalCuts = manualCutter(G,possibleCuts,finalCuts)
+        finalCuts = manualCutter(G, possibleCuts, finalCuts)
     # Automatic cut
-    else: 
+    else:
         for edge in possibleCuts:
             # Ignore candidates if both belongs to same root.
             if G[edge[0]].belongingRoot != G[edge[1]].belongingRoot:
                 ## TEST PURPOSE ONLY ##
                 if PARAMETER:
-                    weakSpot = min(G[edge[0]].abundance, G[edge[1]].abundance)
-                    biggestRoot = max(G[G[edge[0]].belongingRoot].abundance, G[G[edge[1]].belongingRoot].abundance)
-                    smalletsRoot = min(G[G[edge[0]].belongingRoot].abundance, G[G[edge[1]].belongingRoot].abundance)
-                    if (smalletsRoot/weakSpot > 25 and biggestRoot/smalletsRoot < 10) or smalletsRoot / weakSpot > THRESHOLD/2:
+                    weakSpot = min(G[edge[0]].abundance,
+                                   G[edge[1]].abundance)
+                    biggestRoot = max(G[G[edge[0]].belongingRoot].abundance,
+                                      G[G[edge[1]].belongingRoot].abundance)
+                    smalletsRoot = min(G[G[edge[0]].belongingRoot].abundance,
+                                       G[G[edge[1]].belongingRoot].abundance)
+                    if (smalletsRoot/weakSpot > 25
+                        and biggestRoot/smalletsRoot < 10) \
+                            or smalletsRoot / weakSpot > THRESHOLD/2:
                         finalCuts.append(edge)
                 else:
                     finalCuts.append(edge)
-    
+
     ### Print Final Cuts ###
     if finalCuts:
-        tim = time.clock()-tim
-        prettyPrintCuts(G,finalCuts,tim)
+        tim = time.clock() - tim
+        prettyPrintCuts(G, finalCuts, tim)
 
     return finalCuts
 
 
-def computeCuts(G,THRESHOLD):
+def computeCuts(G, THRESHOLD):
 
     ### Measure time ###
     tim = time.clock()
 
     ### Assign a parent (biggest neighbour) to each node ###
-    assignParent(G,THRESHOLD)
+    assignParent(G, THRESHOLD)
 
     ### Find possible cuts ###
     possibleCuts = findPossibleCuts(G)
 
     ### Find belonging root to each node in possibleCuts ###
-    findBelongingRoot(G,possibleCuts)
+    findBelongingRoot(G, possibleCuts)
 
     ### rewire nodes with small belonging roots ###
-    rewireNode(G,possibleCuts,THRESHOLD)
-    
+    rewireNode(G, possibleCuts, THRESHOLD)
+
     ### find final cuts: manually, parameter, or only by threshold ###
-    finalCuts = findFinalCuts(G,possibleCuts,THRESHOLD,tim)
+    finalCuts = findFinalCuts(G, possibleCuts, THRESHOLD, tim)
 
     return finalCuts
 
 
-def findNewSwarms(G,seeds):
+def findNewSwarms(G, seeds):
     ### Performing BFS to find new swarms ###
     print "Performing BFS to discover new swarms..."
     tim = time.clock()
@@ -360,20 +373,21 @@ def findNewSwarms(G,seeds):
             while queue:
                 count += 1
                 currentNode = queue.popleft()
-                new_swarm.append((currentNode,G[currentNode].abundance))
+                new_swarm.append((currentNode, G[currentNode].abundance))
                 for node in G[currentNode].neighbours:
                     if not G[node].checked:
                         queue.append(node)
                         G[node].checked = True
-            
+
             new_swarm.sort(key=itemgetter(1), reverse=True)
             new_swarms.append(new_swarm)
-    print "Visited nodes:",count
-    print "BFS time:",time.clock()-tim
-    print "Num swarms:",len(new_swarms)
+    print "Visited nodes:", count
+    print "BFS time:", time.clock()-tim
+    print "Num swarms:", len(new_swarms)
     if count != len(G):
         print "ERROR: DATA LOST AFTER CUTTING."
     return new_swarms
+
 
 def find_path(graph, start, end, path=[]):
     path = path + [start]
@@ -382,11 +396,12 @@ def find_path(graph, start, end, path=[]):
     for node in graph[start].neighbours:
         if node not in path:
             newpath = find_path(graph, node, end, path)
-            if newpath: return newpath
+            if newpath:
+                return newpath
     return None
 
 
-def breakSwarm(G,THRESHOLD):
+def breakSwarm(G, THRESHOLD):
     """
     Iteratively compute final cuts in the graph.
     Perform the final cut on data structure, and add nodes
@@ -398,17 +413,17 @@ def breakSwarm(G,THRESHOLD):
     while moreCuts:
         iteration += 1
         print "\nRunning iteration "+str(iteration)+":"
-        finalCuts = computeCuts(G,THRESHOLD)
+        finalCuts = computeCuts(G, THRESHOLD)
         if len(finalCuts) == 0:
             moreCuts = False
             print "No more final cuts found\n"
         else:
-            
+
             # For testing - to see paths
             if iteration == 1:
                 print "Path:"
                 #path = find_path(G,12459,5889)
-                path = find_path(G,0,824)
+                path = find_path(G, 0, 824)
                 print path
                 print [G[i].abundance for i in path]
                 print [G[i].belongingRoot for i in path]
@@ -422,9 +437,8 @@ def breakSwarm(G,THRESHOLD):
                 print G[nod].belongingRoot
                 print G[G[nod].belongingRoot].abundance
                 print G[nod].neighbours
-                print [G[G[nod].neighbours[i]].abundance 
-                        for i in range(len(G[nod].neighbours))]
-
+                print [G[G[nod].neighbours[i]].abundance
+                       for i in range(len(G[nod].neighbours))]
 
             ### Performing final cuts on graph structure ###
             tim = time.clock()
@@ -435,28 +449,29 @@ def breakSwarm(G,THRESHOLD):
                         G[node].seed = True
                 G[edge[0]].neighbours.remove(edge[1])
                 G[edge[1]].neighbours.remove(edge[0])
-            print "Making final cuts:",time.clock()-tim
+            print "Making final cuts:", time.clock()-tim
 
     # path = find_path(G,0,1)
     # print path
     # print [G[i].abundance for i in path]
     # print G[1683].neighbours
-    # print [G[G[1683].neighbours[i]].abundance for i in range(len(G[1683].neighbours))]
+    # print [G[G[1683].neighbours[i]].abundance for i in
+    #        range(len(G[1683].neighbours))]
     # print G[G[1683].belongingRoot].abundance
     # print G[1683].belongingRoot
     # print
 
     #print "Seeds:\n", new_swarm_seeds
-    print "Num possible seeds:", len(new_swarm_seeds),"\n"
+    print "Num possible seeds:", len(new_swarm_seeds), "\n"
     return new_swarm_seeds
 
 
-def outputSwarmFile(G,new_swarms,swarm_file):
+def outputSwarmFile(G, new_swarms, swarm_file):
     """
     Output new swarm file
     """
     tim = time.clock()
-    
+
     output_file_swarm = os.path.splitext(swarm_file)[0]+"_new.swarm"
     with open(output_file_swarm, 'w') as f:
         for swarm in new_swarms:
@@ -469,24 +484,24 @@ def outputSwarmFile(G,new_swarms,swarm_file):
 
 
 def main():
-    
+
     ### Set THRESHOLD value ###
     THRESHOLD = 100
-    
+
     ### Parse command line options ###
     fasta_file, swarm_file, input_file = option_parse()
-    
+
     ### Build data structure ###
     G = buildGraph(fasta_file, swarm_file, input_file)
 
     ### Compute cuts and break swarm ###
-    new_swarm_seeds = breakSwarm(G,THRESHOLD)
+    new_swarm_seeds = breakSwarm(G, THRESHOLD)
 
     ### Find new swarms ###
-    new_swarms = findNewSwarms(G,new_swarm_seeds)
-    
+    new_swarms = findNewSwarms(G, new_swarm_seeds)
+
     ### Output new swarm file ###
-    outputSwarmFile(G,new_swarms,swarm_file)
+    outputSwarmFile(G, new_swarms, swarm_file)
 
 
 #*****************************************************************************#
@@ -499,7 +514,7 @@ if __name__ == '__main__':
 
     main()
 
-sys.exit(0)
+    sys.exit(0)
 
 """
 Run example (use -m cProfile, to test time):
